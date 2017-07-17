38e80a1ce21a8744e20e6568bcba13b1ccf1b26b
limozaza <zakboite@gmail.com>
2017-07-18 00:53:09 +0200

    Add SearchComponent to App

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 
 const API_KEY = 'AIzaSyAGjtQu3KOtP344QVK8JpIf6bcr3chXMXs';
 
+import SearchBar from './components/search_bar';
+
 const App = () => {
-    return <div>Bonjour</div>;
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
